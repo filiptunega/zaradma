7abf932b1001519f79e338f637085faa01bc4c9d
Filip Tunega <tunega.f@gmail.com>
2026-04-23 14:57:14 +0200

    Initial Commit

---
+++ zaradma/zaradmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  zaradmaApp.swift
+//  zaradma
+//
+//  Created by Filip Tunega on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct zaradmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
